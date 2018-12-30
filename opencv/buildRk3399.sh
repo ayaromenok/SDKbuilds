8be4c71055ee6f665e0869ce29b25aebc7b1163a
Andrey E. Yaromenok <ayaromenok@gmail.com>
2018-12-30 16:58:59 +0100

    cv/rk3399 - still can build on all 6 cores

--- opencv/buildRk3399.sh
+++ opencv/buildRk3399.sh
@@ -1,10 +1,11 @@
 #default system from friendlyarm required installation of cmake
-#sudo apt install cmake
+sudo apt install cmake
 mkdir ~/sdk
 cd ~/sdk
 mkdir src
 cd ~/sdk/src
 git clone https://github.com/ayaromenok/opencv.git
+git clone https://github.com/opencv/opencv_extra.git &
 mkdir build
 cd build
 mkdir opencv
@@ -12,14 +13,14 @@ cd opencv
 git checkout OpenCL_OutOfRes_ArmMali
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D BUILD_TESTS=ON \
+	-D BUILD_TESTS=OFF \
 	-D BUILD_EXAMPLES=OFF \
 	-D INSTALL_C_EXAMPLES=OFF \
 	-D WITH_OPENCL=ON \
 	../../opencv
-make -j 6
+make -j 2
 sudo make -j 6 install
 #	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
 
 #for opencv_perf_
-#git clone https://github.com/opencv/opencv_extra.git
+#
