6fa360c15b656b2c32584ff92ca55cde12e98744
Andrey Yaromenok <ayaromenok@gmail.com>
2019-01-26 13:46:47 +0200

    rpi/opencv build added

---
+++ opencv/buildRPi3.sh
@@ -0,0 +1,28 @@
+#default system from friendlyarm required installation of cmake
+sudo apt install cmake
+mkdir ~/sdk
+cd ~/sdk
+mkdir src
+cd ~/sdk/src
+git clone https://github.com/ayaromenok/opencv.git
+git clone https://github.com/opencv/opencv_extra.git &
+cd opencv
+git checkout OpenCL_OutOfRes_ArmMali
+cd ..
+mkdir build
+cd build
+mkdir opencv
+cd opencv
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D BUILD_TESTS=OFF \
+	-D BUILD_EXAMPLES=OFF \
+	-D INSTALL_C_EXAMPLES=OFF \
+	-D WITH_OPENCL=ON \
+	../../opencv
+make -j 2
+sudo make -j 6 install
+#	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+
+#for opencv_perf_
+#
