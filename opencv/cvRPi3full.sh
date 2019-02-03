2105ebf3b34a55ad5ab70795826ff9192333fbf3
Andrey Yaromenok <ayaromenok@gmail.com>
2019-02-03 08:08:27 +0200

    full OpenCV for RPi3 added

--- opencv/cvRPi3full.sh
+++ opencv/cvRPi3full.sh
@@ -9,8 +9,10 @@ mkdir src
 cd src
 mkdir opencv
 cd opencv
-git clone https://github.com/ayaromenok/opencv.git
-#git clone https://github.com/opencv/opencv_extra.git &
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+#git clone https://github.com/opencv/opencv_extra.git
+
 cd ..
 mkdir build
 cd build
@@ -21,15 +23,16 @@ echo ">>>> Configure OpenCV"
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D BUILD_TESTS=OFF \
-	-D BUILD_EXAMPLES=OFF \
-	-D INSTALL_C_EXAMPLES=OFF \
+	-D BUILD_EXAMPLES=ON \
+	-D INSTALL_C_EXAMPLES=ON \
 	-D WITH_OPENCL=OFF \
 	-D ENABLE_NEON=ON \
 	-D ENABLE_VFPV3=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=../../opencv/opencv_contrib/modules \
 	../../opencv/opencv
 	
 echo ">>>> It's time for: make & make install & ldconfig"	
 #make
 #sudo make -j 1 install
-#	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+#
 #
