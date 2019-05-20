64d7cef8d6642d98780eb03194eaae7b87e558f6
Andrey Yaromenok/TegraX1 <ayaromenok@gmail.com>
2019-05-20 08:35:14 +0300

    tegraX1: sys/qt5 small fixes

--- hardware/sysTegraX1.sh
+++ hardware/sysTegraX1.sh
@@ -1,4 +1,2 @@
-sudo apt-get build-dep qt5-default
-sudo apt-get install libxcb-xinerama0-dev
-sudo apt-get install libasound2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libvulkan-dev
-sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev cmake
+sudo apt-get build-dep qt5-default 
+sudo apt-get install libxcb-xinerama0-dev libasound2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libvulkan-dev '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev cmake
