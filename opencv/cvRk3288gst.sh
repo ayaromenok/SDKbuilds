e3583eb5f1656e0a09b595339ad0a5c50bd3bff8
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-05-23 19:05:05 +0300

    rk3288/TinkerBoard OS gstreamer libs added

---
+++ opencv/cvRk3288gst.sh
@@ -0,0 +1,5 @@
+# gstreamer for Rk3288 prerequiremens
+sudo apt install libgtk2.0-dev pkg-config cmake gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-base-apps libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev \
+    autopoint libtool  glib-2.0-dev libglib2.0-dev libglib2.0-bin libglib2.0-dev  libgstreamer1.0-dev \
+    libgstreamer-plugins-base1.0-dev
