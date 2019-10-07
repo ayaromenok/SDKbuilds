9b71c18142ca085541ecf7ab86542e1e00a550b5
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-10-07 08:40:14 +0200

    #9 Mesa script updated

--- mesa/rpi4_vc6.sh
+++ mesa/rpi4_vc6.sh
@@ -1,7 +1,22 @@
 sudo apt-get install build-essential git meson
 sudo apt-get build-dep mesa
 
+mkdir ~/sdk
+cd ~/sdk
+mkdir src
+cd src
+
 git clone https://gitlab.freedesktop.org/anholt/mesa.git
 cd mesa
 git checkout v3d-opt-sub
 #git checkout v3d-cs
+meson build \
+	--prefix=/usr \
+	--libdir=/usr/lib/arm-linux-gnueabihf \
+	-Dbuildtype=release \
+	-Dplatforms=x11,drm,surfaceless \
+	-Ddri-drivers= \
+	-Dgallium-drivers=v3d
+
+ninja -C build \
+sudo -E ninja -C build install
