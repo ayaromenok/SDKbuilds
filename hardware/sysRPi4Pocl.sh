732705238bc65f883158754dbd4a055daf72849c
Andrey Yaromenok/RPi4 <ayaromenok@gmail.com>
2019-10-08 11:19:11 +0200

    POCL for RPi4 build

---
+++ hardware/sysRPi4Pocl.sh
@@ -0,0 +1,24 @@
+sudo apt install cmake clang-7 libclang-7-dev libhwloc-dev hwloc
+sudo ln -s /usr/bin/llvm-config-7 /usr/bin/llvm-config
+mkdir ~/sdk
+cd  ~/sdk
+mkdir src
+cd src
+mkdir pocl
+cd pocl
+
+wget https://github.com/pocl/pocl/archive/v1.3.tar.gz
+tar xf v1.3.tar.gz
+
+cd ../..
+mkdir build
+cd build
+mkdir pocl
+cd pocl
+
+cmake \
+../../src/pocl/pocl-1.3
+
+make -j 4
+sudo make install
+sudo ldconfig
