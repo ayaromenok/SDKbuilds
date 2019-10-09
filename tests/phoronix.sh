3c91c96f554b0e0ddc56a7a1b7c381b5aa69da20
Andrey Yaromenok/RPi4 <ayaromenok@gmail.com>
2019-10-09 13:55:31 +0200

    remove *deb in phoronix after installation

--- tests/phoronix.sh
+++ tests/phoronix.sh
@@ -1,7 +1,7 @@
 sudo apt install php-cli php-gd php-xml php-zip inxi
-rm -f *.deb
 wget http://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_9.0.1_all.deb
 sudo dpkg -i phoronix-test-suite_9.0.1_all.deb
+rm -f *.deb
 
 inxi -Fc0
 phoronix-test-suite benchmark 1709271-TY-1704029RI26
