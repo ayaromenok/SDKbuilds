02fb45f586ffba6cc892502df2a68f9e704e7bcc
Andrey Yaromenok/RPi4 <ayaromenok@gmail.com>
2019-10-09 13:56:16 +0200

    #11 added glmark2 with GL2 and GLESv2

---
+++ tests/glmark2.sh
@@ -0,0 +1,18 @@
+mkdir ~/sdk
+cd ~/sdk
+mkdir tools
+cd tools
+mkdir tests
+cd tests
+git clone https://github.com/glmark2/glmark2.git
+cd glmark2
+./waf configure --with-flavors=x11-glesv2 --destdir=/usr/local
+./waf
+sudo ./waf
+./waf configure --with-flavors=x11-gl --destdir=/usr/local
+./waf
+sudo ./waf
+cd ..
+sudo rm -r glmark2
+touch glmark2.installed
+touch glmark2-es2.installed
