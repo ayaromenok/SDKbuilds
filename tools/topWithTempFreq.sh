86c261f13d94ec1c461d9cef99ef8eac57e683f0
Andrey Yaromenok/rc3 <ayaromenok@gmail.com>
2019-10-18 09:28:33 +0200

    added autotools as prerequirement to build htop with temperature

--- tools/topWithTempFreq.sh
+++ tools/topWithTempFreq.sh
@@ -7,7 +7,7 @@ mkdir tools
 cd tools
 git clone https://github.com/wbenny/htop.git
 cd htop
-sudo apt install libncurses5-dev
+sudo apt install libncurses5-dev build-essential autogen autoconf automake autotools-dev gettext dh-autoreconf
 ./autogen.sh
 ./configure --disable-unicode
 make -j 4
