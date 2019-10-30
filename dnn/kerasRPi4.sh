1603908c8b74f5bc1667a9991843fee981df449e
Andrey Yaromenok/RPi4 <ayaromenok@gmail.com>
2019-10-30 18:32:09 +0100

    keras/tf for RPi4 added

---
+++ dnn/kerasRPi4.sh
@@ -0,0 +1,8 @@
+echo "RPi4/keras"
+sudo apt update
+sudo apt-get install python3.6-dev libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
+sudo apt-get install python3-pip
+sudo pip3 install -U pip
+sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
+sudo pip3 install tensorflow
+sudo pip3 install keras
