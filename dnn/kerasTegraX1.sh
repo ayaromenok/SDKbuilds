8ee58645b2c9ca0ff77c62259b6ce3c27800af64
Andrey Yaromenok/TegraX1 <ayaromenok@gmail.com>
2019-10-31 18:28:40 +0100

    update for Keras on TegraX1

--- dnn/kerasTegraX1.sh
+++ dnn/kerasTegraX1.sh
@@ -2,6 +2,7 @@ echo "Py 3.6+JetPack4.2.1"
 sudo apt update
 sudo apt-get install python3.6-dev libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
 sudo apt-get install python3-pip
+sudo apt-get install python3-gi-cairo
 sudo pip3 install -U pip
 sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.14.0+nv19.9
