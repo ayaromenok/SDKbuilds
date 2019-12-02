a843b444a96638912ce2a5044387e21282098bdc
Andrey Yaromenok/TegraX1 <ayaromenok@gmail.com>
2019-12-02 10:32:52 +0100

    pocl on tegra updated to 1.4, with clang 8

--- hardware/sysTegraX1Pocl.sh
+++ hardware/sysTegraX1Pocl.sh
@@ -1,5 +1,5 @@
-sudo apt install cmake clang-7 libclang-7-dev libhwloc-dev hwloc
-sudo ln -s /usr/bin/llvm-config-7 /usr/bin/llvm-config
+#sudo apt install cmake clang-7 libclang-7-dev libhwloc-dev hwloc
+#sudo ln -s /usr/bin/llvm-config-7 /usr/bin/llvm-config
 mkdir ~/sdk
 cd  ~/sdk
 mkdir src
@@ -7,8 +7,8 @@ cd src
 mkdir pocl
 cd pocl
 
-wget https://github.com/pocl/pocl/archive/v1.3.tar.gz
-tar xf v1.3.tar.gz
+wget https://github.com/pocl/pocl/archive/v1.4.tar.gz
+tar xf v1.4.tar.gz
 
 cd ../..
 mkdir build
@@ -18,4 +18,4 @@ cd pocl
 
 cmake \
 -D ENABLE_CUDA=ON \
-../../src/pocl/pocl-1.3
+../../src/pocl/pocl-1.4
