382ac042c8a4e43628e6d0f0b4e555dddb6f45aa
Andrey Yaromenok <ayaromenok@gmail.com>
2020-10-01 04:48:11 -0400

    update Qt for RPi3/VC4 with Qt 5.15.1

--- qt/qtRPi3_vc4.sh
+++ qt/qtRPi3_vc4.sh
@@ -3,7 +3,7 @@ echo ">>>> Setup Qt5 all"
 #../hardware/sysRPi3.sh
 
 #sudo apt-get build-dep qtbase-opensource-src qt-opensource-src
-#sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev
+#sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libgles2-mesa-dev libegl1-mesa-dev
 #for multimedia, probably plugins-bad required - need to test
 #sudo apt-get install libasound2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
 
@@ -16,8 +16,8 @@ cd src
 mkdir qt
 cd qt
 
-#wget http://download.qt.io/official_releases/qt/5.12/5.12.5/single/qt-everywhere-src-5.12.5.tar.xz
-#tar xf qt-everywhere-src-5.12.5.tar.xz
+#wget http://download.qt.io/official_releases/qt/5.15/5.15.1/single/qt-everywhere-src-5.15.1.tar.xz
+#tar xf qt-everywhere-src-5.15.1.tar.xz
 
 cd ../..
 mkdir build
@@ -27,8 +27,8 @@ cd qt_vc4
 
 echo ">>>> Configure Qt5 all"
 PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/share/pkgconfig \
-../../src/qt/qt-everywhere-src-5.12.5/configure \
--opengl desktop \
+../../src/qt/qt-everywhere-src-5.15.1/configure \
+-opengl es2 \
 -no-gtk \
 -opensource -confirm-license -release \
 -reduce-exports \
@@ -56,6 +56,9 @@ PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/share/pkgconfig \
 -skip qtscxml \
 -skip qttools \
 -skip qtxmlpatterns \
+-skip qtquick3d \
+-skip qtserialport \
+-skip qtserialbus \
 -prefix /usr/local
 
 QMAKE_CFLAGS="-march=armv8-a -mtune=cortex-a53 -mfpu=crypto-neon-fp-armv8" \
@@ -64,6 +67,7 @@ QMAKE_CXXFLAGS="-march=armv8-a -mtune=cortex-a53 -mfpu=crypto-neon-fp-armv8" \
 
 echo ">>>> RPi X11 status:"
 less config.summary | grep "Desktop OpenGL"
+less config.summary | grep "OpenGL ES"
 less config.summary | grep "XCB Xlib"
 less config.summary | grep "EGL on X11"
 	
