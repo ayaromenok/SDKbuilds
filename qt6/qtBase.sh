fadb8248672a34e835dfe461d7f1e095359360ce
Andrey Yaromenok <ayaromenok@gmail.com>
2020-10-23 07:05:36 +0300

    qt6 build script added

---
+++ qt6/qtBase.sh
@@ -0,0 +1,35 @@
+QT6_VER=6.0.0-beta1
+SRC_DIR=~/sdk/src/qt
+BUILD_DIR=~/sdk/build/qtBase
+
+echo "Install Qt ${QT6_VER}, src in ${SRC_PATH}, build in ${BUILD_PATH}"
+#sudo apt-get build-dep qt5-default qtdeclarative5-dev cmake -y
+#sudo apt-get install libxcb-xinerama0-dev libasound2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libvulkan-dev '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev cmake libxkbcommon-dev libxkbcommon-x11-dev -y
+
+
+echo ">>>> get Qt6 Base - ${QT6_VER}"
+mkdir "${SRC_DIR}" -p
+cd ${SRC_DIR}
+
+QT6_SRC_DIR=qtbase-everywhere-src-${QT6_VER}
+if test -d "$QT6_SRC_DIR"; then
+    echo "$QT6_SRC_DIR already exists."
+else
+    echo "$QT6_SRC_DIR does not exists."
+    wget http://download.qt.io/development_releases/qt/6.0/${QT6_VER}/submodules/qtbase-everywhere-src-${QT6_VER}.tar.xz
+    echo "Extracting Qt-${QT6_VER}"
+    tar xf qtbase-everywhere-src-${QT6_VER}.tar.xz
+    rm  qtbase-everywhere-src-${QT6_VER}.tar.xz
+    echo "Done"
+fi
+
+QT6_BUILD_DIR=${BUILD_DIR}-${QT6_VER}
+if test -d "$QT6_BUILD_DIR"; then
+    echo "$QT6_BUILD_DIR already exists. Remove it to rebuild from scratch."
+else
+    echo "$QT6_BUILD_DIR does not exists."
+    mkdir "${QT6_BUILD_DIR}"
+    cd  "${QT6_BUILD_DIR}"
+fi
+
+
