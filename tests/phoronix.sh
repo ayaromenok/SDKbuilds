b013423d5edd6fe19e3ef6cd64724dfdb3f5c44a
Andrey Yaromenok <ayaromenok@gmail.com>
2021-04-09 08:43:38 +0300

    PTS updated to 10.x

--- tests/phoronix.sh
+++ tests/phoronix.sh
@@ -1,7 +1,7 @@
 sudo apt install php-cli php-gd php-xml php-zip inxi
-wget http://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_9.0.1_all.deb
-sudo dpkg -i phoronix-test-suite_9.0.1_all.deb
+wget http://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_10.2.2_all.deb
+sudo dpkg -i phoronix-test-suite_10.2.2_all.deb
 rm -f *.deb
 
 inxi -Fc0
-phoronix-test-suite benchmark 1709271-TY-1704029RI26
+#phoronix-test-suite benchmark 1709271-TY-1704029RI26
