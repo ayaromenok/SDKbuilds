aa8fec4bed647aa4718676f98a81d166de0b38b7
Andrey Yaromenok <ayaromenok@gmail.com>
2021-04-26 07:23:48 +0300

    clear build for rpi3/4

--- opencv/cvRPi3full.sh
+++ opencv/cvRPi3full.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo ">>>> Setup OpenCV on RPi3"
 ../hardware/sysRPi3.sh
+sudo mv /usr/bin/python /usr/bin/_python
+sudo mv /usr/bin/python3 /usr/bin/_python3
 
 echo ">>>> Get OpenCV"
 mkdir ~/sdk
@@ -25,9 +27,12 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D BUILD_TESTS=ON \
 	-D BUILD_PERF_TESTS=ON \
 	-D BUILD_EXAMPLES=ON \
+	-D BUILD_JAVA=OFF \
+	-D BUILD_PYTHON=OFF \
 	-D INSTALL_C_EXAMPLES=ON \
 	-D WITH_OPENCL=OFF \
 	-D ENABLE_NEON=ON \
+	-D WITH_GSTREAMER=ON \
 	-D ENABLE_VFPV3=ON \
 	-D WITH_QT=ON \
 	-D WITH_OPENGL=OFF\
@@ -37,5 +42,8 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 echo ">>>> It's time for: make & make install & ldconfig"	
 make -j 2
 sudo make -j 4 install
+sudo mv /usr/bin/_python /usr/bin/python
+sudo mv /usr/bin/_python3 /usr/bin/python3
+
 #
 #
