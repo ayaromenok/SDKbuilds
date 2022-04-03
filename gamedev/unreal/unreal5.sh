f5b7d6d3cf207da5b6acbd246722a7e74e24f2f0
Andrey Yaromenok <ayaromenok@gmail.com>
2022-04-03 09:18:13 -0400

    #14 added libssl 1.1 for ue5/setup.sh

--- gamedev/unreal/unreal5.sh
+++ gamedev/unreal/unreal5.sh
@@ -1,3 +1,6 @@
+echo "UE5 required libssl 1.x to run setup.sh -to  avoid error: No usable version of libssl was found
+"
+sudo apt install libssl1.1 -y
 mkdir ~/sdk
 cd ~/sdk
 git clone git@github.com:EpicGames/UnrealEngine.git --branch ue5-main --single-branch --depth 1 ue5
