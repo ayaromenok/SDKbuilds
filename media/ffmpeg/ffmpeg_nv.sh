2774ddfa3d0f9f9defd6c50bbc3d6563160c8b74
Andrey Yaromenok <ayaromenok@gmail.com>
2022-04-03 08:26:50 -0400

    #15 move mmpeg and gstreamer to media section

---
+++ media/ffmpeg/ffmpeg_nv.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt update
+sudo apt install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev -y
+
+mkdir ~/sdk/src/ffmpeg
+cd ~/sdk/src/ffmpeg
+
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+git clone https://git.ffmpeg.org/ffmpeg.git
+
+cd nv-codec-headers && sudo make install && cd - 
+mkdir ~/sdk/build/ffmpeg
+cd ~/sdk/build/ffmpeg
+
+../../src/ffmpeg/ffmpeg/configure --enable-nonfree --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+
+make -j 4
+
+sudo make install
