8674ddb80b2e4e5b92eaddebe71d750392eae926
Andrey Yaromenok <ayaromenok@gmail.com>
2022-06-01 22:29:57 +0300

    riscv: qemu added start with RVV 1.0

---
+++ hardware/riscv/qemu-riscv64rvv.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/qemu-riscv64 -cpu rv64,v=true,vlen=128,elen=64,vext_spec=v1.0 $1
--- hardware/riscv/qemu.sh
+++ hardware/riscv/qemu.sh
@@ -6,9 +6,11 @@ cd riscv
 sudo apt install qemu-system-misc qemu-user-static binfmt-support ninja-build opensbi u-boot-qemu -y
 git clone https://git.qemu.org/git/qemu.git
 cd qemu
-./configure --static --disable-system --target-list=riscv64-linux-user
+#./configure --static --disable-system --target-list=riscv64-linux-user
+./configure --target-list=riscv64-linux-user
 make -j 4
-sudo cp riscv64-linux-user/qemu-riscv64 /usr/bin/qemu-riscv64-static
+sudo make install
+sudo cp ./qemu-riscv64rvv.sh /usr/local/bin/
 
 
 cat >/tmp/qemu-riscv64 <<EOF
@@ -17,7 +19,7 @@ type magic
 offset 0
 magic \x7f\x45\x4c\x46\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xf3\x00
 mask \xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff
-interpreter /usr/bin/qemu-riscv64-static
+interpreter /usr/local/bin/qemu-riscv64rvv.sh
 EOF
 
 sudo update-binfmts --import /tmp/qemu-riscv64
