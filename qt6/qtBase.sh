d0f0d2fb046022508ecc7079466abd88a6ce78be
Andrey Yaromenok <ayaromenok@gmail.com>
2022-08-19 18:15:49 +0300

    qt updates

--- qt6/qtBase.sh
+++ qt6/qtBase.sh
@@ -6,10 +6,10 @@ BUILD_DIR=~/sdk/build/qt${QT6_VER_MAJOR}/base
 echo "Install Qt ${QT6_VER}, src in ${SRC_PATH}, build in ${BUILD_PATH}"
 sudo apt-get build-dep qt5-default qtdeclarative5-dev -y
 sudo apt-get install build-essential git libxcb-xinerama0-dev libasound2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libvulkan-dev '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y
-sudo apt-get install  libxcb-xinput-dev libxi-dev libx11-xcb-dev
-sudo apt install libmd4c-dev libdouble-conversion-dev
+sudo apt-get install  libxcb-xinput-dev libxi-dev libx11-xcb-dev -y
+sudo apt install libmd4c-dev libdouble-conversion-dev -y
 sudo ln -s /usr/lib/x86_64-linux-gnu/libmd4c.so.0.4.8 /usr/lib/x86_64-linux-gnu/libmd4c-html.so.0.4.8
-sudo apt-get install libxkbcommon-x11-dev
+sudo apt-get install libxkbcommon-x11-dev -y
 #sudo apt-get install cmake
 
 echo ">>>> get Qt6 Base - ${QT6_VER}"
--- tests/phoronix.sh
+++ tests/phoronix.sh
@@ -1,6 +1,6 @@
 sudo apt install php-cli php-gd php-xml php-zip inxi
-wget http://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_10.2.2_all.deb
-sudo dpkg -i phoronix-test-suite_10.2.2_all.deb
+wget http://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_10.8.3_all.deb
+sudo dpkg -i phoronix-test-suite_10.8.3_all.deb
 rm -f *.deb
 
 inxi -Fc0
