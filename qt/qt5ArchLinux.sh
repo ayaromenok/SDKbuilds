6f65f746d0352be56dbacf7c80494a3ebb8a8046
Andrey Yaromenok <ayaromenok@gmail.com>
2022-11-30 10:49:09 +0000

    qt5 for Arch Linux added

---
+++ qt/qt5ArchLinux.sh
@@ -0,0 +1,11 @@
+paru -S gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
+paru -S qt5-base qt5-3d qt5-charts qt5-connectivity qt5-datavis3d \
+		qt5-declarative qt5-doc qt5-examples qt5-feedback qt5-gamepad \
+		qt5-graphicaleffects qt5-gsettings qt5-imageformats qt5-location qt5-lottie qt5-multimedia \
+		qt5-networkauth qt5-purchasing qt5-quick3d qt5-quickcontrols qt5-quickcontrols2 \
+		qt5-quicktimeline qt5-remoteobjects qt5-script qt5-scxml qt5-sensors \
+		qt5-serialbus qt5-serialport qt5-speech qt5-svg qt5-tools \
+		qt5-virtualkeyboard qt5-webchannel qt5-webengine qt5-webglplugin \
+		qt5-websockets qt5-webview qt5-x11extras qt5-xmlpatterns
+
+#qt5-translations qt5-wayland qt5-xcb-private-headers
