5c09a41d345b81e374a3eacb0adde7fb8cfaf9aa
Andrey Yaromenok <ayaromenok@gmail.com>
2022-11-30 10:14:53 +0000

    qt6 for Arch Linux

---
+++ qt6/qt6ArchLinux.sh
@@ -0,0 +1,9 @@
+paru -S gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav 
+paru -S qt6-base qt6-3d  qt6-qt5compat qt6-charts qt6-connectivity qt6-datavis3d qt6-declarative qt6-doc qt6-examples \
+        qt6-httpserver qt6-imageformats qt6-languageserver qt6-lottie qt6-multimedia qt6-multimedia-ffmpeg qt6-multimedia-gstreamer \
+	qt6-networkauth qt6-positioning qt6-quick3d qt6-quick3dphysics qt6-quicktimeline \
+	qt6-remoteobjects qt6-scxml qt6-sensors qt6-serialbus qt6-serialport \
+	qt6-shadertools qt6-speech qt6-svg qt6-tools qt6-virtualkeyboard \
+	qt6-webchannel qt6-webengine qt6-websockets qt6-webview
+
+#qt6-translations	qt6-wayland	
