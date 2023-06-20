292e865b076879792b5289c617d8db785d34a0ec
Andrey Yaromenok <ayaromenok@gmail.com>
2023-06-20 12:39:47 +0300

    up: OpenCV - DNN Cuda build

--- opencv/cvPCfullCuda.sh
+++ opencv/cvPCfullCuda.sh
@@ -3,7 +3,7 @@ echo ">>>> Setup OpenCV on PC with OpenCL\CUDA"
 # only basic setup from tegra used - POCL not necessary on PC
 #../hardware/sysTegraX1.sh
 #get vulkan SDK
-sudo apt install lunarg-vktrace lunarg-vkconfig lunarg-vulkan-layers -y
+sudo apt install lunarg-vktrace lunarg-vkconfig lunarg-vulkan-layers libgtk2.0-dev libgtk-3-dev pkg-config -y
 
 echo ">>>> Get OpenCV"
 mkdir ~/sdk
@@ -12,9 +12,9 @@ mkdir src
 cd src
 mkdir opencv
 cd opencv
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-git clone https://github.com/opencv/opencv_extra.git
+#git clone https://github.com/opencv/opencv.git
+#git clone https://github.com/opencv/opencv_contrib.git
+#git clone https://github.com/opencv/opencv_extra.git
 
 
 cd ../..
@@ -27,25 +27,35 @@ echo ">>>> Configure OpenCV"
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D BUILD_PERF_TESTS=ON \
-	-D BUILD_EXAMPLES=ON \
 	-D BUILD_TESTS=OFF \
+	-D BUILD_EXAMPLES=ON \
+	-D BUIL_opencv_apps=ON \
 	-D INSTALL_C_EXAMPLES=ON \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
 	-D OPENCV_GENERATE_PKGCONFIG=YES \
+	-D BUILD_opencv_python3=ON \
 	-D WITH_OPENCL=ON \
 	-D WITH_QT=ON \
+	-D WITH_GTK_2_X=ON \
 	-D WITH_OPENGL=ON \
 	-D WITH_CUDA=ON \
+	-D OPENCV_DNN_CUDA=ON \
+	-D CUDA_ARCH_BIN=6.1 \
+	-D CUDA_ARCH_PTX=6.1 \
+	-D CUDA_FAST_MATH=ON \
 	-D WITH_CUFFT=ON \
 	-D WITH_CUBLAS=ON \
 	-D WITH_CUDNN=ON \
 	-D WITH_VULKAN=ON \
-	-D CUDA_FAST_MATH=ON \
 	-D WITH_CABLAS=ON \
 	-D OPENCV_ENABLE_NONFREE=ON \
 	-D OPENCV_EXTRA_MODULES_PATH=../../src/opencv/opencv_contrib/modules \
 	-D BUILD_opencv_cvv=OFF \
 	-D BUILD_opencv_rgbd=OFF \
 	../../src/opencv/opencv
+
+#	-D CUDA_ARCH_BIN=3.5,6.1 \
+#	-D CUDA_ARCH_PTX=3.5,6.1 \
 
 #	-D BUILD_TESTS=ON \	
 echo ">>>> It's time for: make & make install & ldconfig"	
